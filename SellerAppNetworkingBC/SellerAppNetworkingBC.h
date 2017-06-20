b5013f59862116bb916c9f247665a3080b0a5dd1
Alejandro Hernandez <alejhernandez@deloittemx.com>
2017-06-20 16:58:42 -0500

    Initial Commit

---
+++ SellerAppNetworkingBC/SellerAppNetworkingBC.h
@@ -0,0 +1,19 @@
+//
+//  SellerAppNetworkingBC.h
+//  SellerAppNetworkingBC
+//
+//  Created by Alejandro Hernandez on 20/06/17.
+//  Copyright © 2017 Liverpool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SellerAppNetworkingBC.
+FOUNDATION_EXPORT double SellerAppNetworkingBCVersionNumber;
+
+//! Project version string for SellerAppNetworkingBC.
+FOUNDATION_EXPORT const unsigned char SellerAppNetworkingBCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SellerAppNetworkingBC/PublicHeader.h>
+
+
