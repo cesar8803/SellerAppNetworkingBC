8eea748f2b669f12f6b696d09ac2b01377f00230
Bernardino Guerrero Ibarra <bernardino.guerrero@icloud.com>
2017-07-05 19:14:33 -0500

    subtotal object mapper

--- SellerAppNetworkingBC/BridgeCoreClasses/Objects/SubTotal.swift
+++ SellerAppNetworkingBC/BridgeCoreClasses/Objects/SubTotal.swift
@@ -6,4 +6,18 @@
 //  Copyright © 2017 Liverpool. All rights reserved.
 //
 
+//SubTotal
+
 import Foundation
+import ObjectMapper
+
+public class SubTotal: Mappable{
+    public var number: String?
+    
+    required public init?(map: Map){
+    }
+    
+    public func mapping(map: Map){
+        number <- map["number"]
+    }
+}
