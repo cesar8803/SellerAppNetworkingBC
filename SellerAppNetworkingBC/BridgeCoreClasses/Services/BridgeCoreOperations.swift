aeaa36bed7b8a487fbbf18b503427e6d680c1838
Bernardino Guerrero Ibarra <bernardino.guerrero@icloud.com>
2017-07-25 17:19:23 -0500

    Common enum and opertaions was separated of Withdrawals file

---
+++ SellerAppNetworkingBC/BridgeCoreClasses/Services/BridgeCoreOperations.swift
@@ -0,0 +1,9 @@
+//
+//  BridgeCoreOperations.swift
+//  SellerAppNetworkingBC
+//
+//  Created by Bernardino Guerrero Ibarra on 7/25/17.
+//  Copyright © 2017 Liverpool. All rights reserved.
+//
+
+import Foundation
