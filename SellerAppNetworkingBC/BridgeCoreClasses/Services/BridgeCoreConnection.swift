8fa94b2148d42862966b7e809425599e54444daf
Bernardino Guerrero Ibarra <bernardino.guerrero@icloud.com>
2017-08-22 20:49:06 -0500

    Class to handle BC connection

---
+++ SellerAppNetworkingBC/BridgeCoreClasses/Services/BridgeCoreConnection.swift
@@ -0,0 +1,13 @@
+//
+//  BridgeCoreConnection.swift
+//  SellerAppNetworkingBC
+//
+//  Created by Bernardino Guerrero Ibarra on 8/22/17.
+//  Copyright © 2017 Liverpool. All rights reserved.
+//
+
+import Foundation
+
+class BridgeCoreConnection{
+    
+}
