07790279914a3ddee27f61290c9dfe6733f85f46
tachiMyMobile <oamorales@grupoasesores.com.mx>
2017-10-25 20:40:05 -0500

    Cast in Selectedinstallmentamount Payload Ticket
    Cast in Selectedinstallmentamount Payload Ticket

--- SellerAppNetworkingBC/BridgeCoreClasses/Objects/Ticket.swift
+++ SellerAppNetworkingBC/BridgeCoreClasses/Objects/Ticket.swift
@@ -747,12 +747,20 @@ public class Eglobalcard: Mappable{
 }
 
 public class Selectedinstallmentamount: Mappable{
-    public var number: Int?
+    public var number: Float?
     
     required public init?(map: Map){
     }
     public func mapping(map: Map){
-        number <- map["number"]
+        
+        switch map["number"].currentValue {
+        case (let v as Float):
+            number = v
+        case (let v as String):
+            number = Float(v)
+        default:
+            debugPrint("You should add another kind of data")
+        }
     }
 }
 
