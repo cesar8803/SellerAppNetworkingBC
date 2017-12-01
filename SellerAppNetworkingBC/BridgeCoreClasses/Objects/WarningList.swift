97d107f6d368d2a3f2068f5a8d841936825a977f
ivangcL01 <igarciac01@liverpool.com.mx>
2017-12-01 10:41:42 -0600

    WarningList

---
+++ SellerAppNetworkingBC/BridgeCoreClasses/Objects/WarningList.swift
@@ -0,0 +1,32 @@
+//
+//  WarningList.swift
+//  SellerAppNetworkingBC
+//
+//  Created by HAROJASD on 12/1/17.
+//  Copyright © 2017 Liverpool. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+public class WarningList: Mappable{
+    public var warrantyData: Warrantydata?
+    
+    public required init?(map: Map){
+    }
+    public func mapping(map: Map){
+        warrantyData <- map["warningData"]
+    }
+}
+
+public class WarningData: Mappable{
+    public var message: String?
+    public var large: Bool?
+    
+    public required init?(map: Map){
+    }
+    public func mapping(map: Map){
+        message <- map["message"]
+        large <- map["large"]
+    }
+}
