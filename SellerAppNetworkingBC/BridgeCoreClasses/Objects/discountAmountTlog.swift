8a6855d8b63bdbe9b886fdce3c8bbf8770b84ea4
Unknown <oamorales@grupoasesores.com.mx>
2018-03-14 11:13:59 -0600

    Map Prorateo
    Map Prorateo

---
+++ SellerAppNetworkingBC/BridgeCoreClasses/Objects/discountAmountTlog.swift
@@ -0,0 +1,13 @@
+//
+//  discountAmountTlog.swift
+//  SellerAppNetworkingBC
+//
+//  Created by Oscar Morales on 14/03/18.
+//  Copyright © 2018 Liverpool. All rights reserved.
+//
+
+import UIKit
+
+class discountAmountTlog: NSObject {
+
+}
---
+++ SellerAppNetworkingBC/BridgeCoreClasses/Objects/proratedDiscountsTotal.swift
@@ -0,0 +1,13 @@
+//
+//  proratedDiscountsTotal.swift
+//  SellerAppNetworkingBC
+//
+//  Created by Oscar Morales on 14/03/18.
+//  Copyright © 2018 Liverpool. All rights reserved.
+//
+
+import UIKit
+
+class proratedDiscountsTotal: NSObject {
+
+}
