da7bd97b74359a483ea1f164d82a72ea9aeadbba
Oscar Morales <oamorales@grupoasesores.com.mx>
2019-01-07 16:51:36 -0600

    map public objects
    map public objects

--- SellerAppNetworkingBC/BridgeCoreClasses/MapObjects/AdditionalDataRepresentation.swift
+++ SellerAppNetworkingBC/BridgeCoreClasses/MapObjects/AdditionalDataRepresentation.swift
@@ -9,15 +9,15 @@
 import Foundation
 import ObjectMapper
 
-class AdditionalDataRepresentation: Mappable {
-    var exceptionIndex : Int?
-    var fieldRequests : [FieldRequests]?
-    var aditionalParameters : [String]?
+public class AdditionalDataRepresentation: Mappable {
+    public var exceptionIndex : Int?
+    public var fieldRequests : [FieldRequests]?
+    public var aditionalParameters : [String]?
     
-    required init?(map: Map){
+    required public init?(map: Map){
     }
     
-    func mapping(map: Map){
+    public func mapping(map: Map){
         exceptionIndex <- map["exceptionIndex"]
         fieldRequests <- map["fieldRequests"]
         aditionalParameters <- map["aditionalParameters"]
--- SellerAppNetworkingBC/BridgeCoreClasses/MapObjects/ExceptionsListData.swift
+++ SellerAppNetworkingBC/BridgeCoreClasses/MapObjects/ExceptionsListData.swift
@@ -10,7 +10,7 @@ import Foundation
 import ObjectMapper
 
 public class ExceptionsListData: Mappable {
-    var additionalDataRepresentation: AdditionalDataRepresentation?
+    public var additionalDataRepresentation: AdditionalDataRepresentation?
     
     required public init?(map: Map){
     }
